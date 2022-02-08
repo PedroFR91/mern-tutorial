39d2d23afb5e7ceeb043dd4c71e44260d55f49b2
DevPed <pedromfr91@gmail.com>
2022-02-08 04:08:06 +0100

    Goal Controller

---
+++ backend/controllers/goalControllers.js
@@ -0,0 +1,30 @@
+//@desc Get goals
+//@route GET /api/goals
+//@acces Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: 'Get goals' });
+};
+//@desc Set goal
+//@route POST /api/goals
+//@acces Private
+const setGoal = (req, res) => {
+  res.status(200).json({ message: 'Set goal' });
+};
+//@desc Update goal
+//@route PUT /api/goals/:id
+//@acces Private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+//@desc Delete goal
+//@route DELETE /api/goals/:id
+//@acces Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
