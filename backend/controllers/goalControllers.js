6ff76d5a9ca7ca37614efef342162e1346955767
DevPed <pedromfr91@gmail.com>
2022-02-08 04:13:49 +0100

    Accepting body data

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -8,6 +8,7 @@ const getGoals = (req, res) => {
 //@route POST /api/goals
 //@acces Private
 const setGoal = (req, res) => {
+  console.log(req.body);
   res.status(200).json({ message: 'Set goal' });
 };
 //@desc Update goal
