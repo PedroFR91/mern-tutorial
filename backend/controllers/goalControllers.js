0d57f5ab69db17917a9451588cb93b8abe587094
DevPed <pedromfr91@gmail.com>
2022-02-13 23:48:10 +0100

    Set Goal

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -17,7 +17,10 @@ const setGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Please add a text field');
   }
-  res.status(200).json({ message: 'Set goal' });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(200).json(goal);
 });
 //@desc Update goal
 //@route PUT /api/goals/:id
