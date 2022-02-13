60d5a9a75fd1f646f71bb95f473685c62fee17e1
DevPed <pedromfr91@gmail.com>
2022-02-13 23:32:13 +0100

    Get Goals

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,9 +1,13 @@
 const asyncHandler = require('express-async-handler');
+
+const Goal = require('../models/goalModel');
+
 //@desc Get goals
 //@route GET /api/goals
 //@acces Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get goals' });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 //@desc Set goal
 //@route POST /api/goals
