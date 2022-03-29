243cb240f19639b54dee03668f96a659585374c5
DevPed <pedromfr91@gmail.com>
2022-03-29 05:50:26 +0200

    Goal Slice

---
+++ frontend/src/features/goals/goalSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  goals: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+};
+
+//Create new goal
+
+export const createGoal = createAsyncThunk(
+  'goals/create',
+  async (goalData, thunkAPI) => {
+    try {
+    } catch (error) {}
+  }
+);
+
+export const goalSlice = createSlice({
+  name: 'goal',
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+});
+
+export const { reset } = goalSlice.actions;
+export default goalSlice.reducer;
